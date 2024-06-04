161d99016be6cbd5231ed20396c3c7c343011ef0
kaiquethainan <kaique.souza104@etec.sp.gov.br>
2024-06-04 20:37:35 -0300

    alteração

--- atividade.c
+++ atividade.c
@@ -3,38 +3,39 @@
 void soma (float num1, float num2){
 	float resultado;
 		resultado= num1 + num2;
-	printf("O resultado é:%2.f", resultado);
+	printf("O resultado é:%.2f", resultado);
 	
 }
 
 void sub (float num1, float num2){
         float resultado;
    	resultado= num1 - num2;
-	printf("o resultado é:%2.f", resultado);
+	printf("o resultado é:%.2ff", resultado);
 }
 
 void mult (float num1, float num2){
 	float resultado;
 	resultado= num1 * num2;
-	printf("o resultado é:%2.f", resultado);
+	printf("o resultado é:%.2f", resultado);
 
 }
 
 void div (float num1, float num2){
 	float resultado;
-	//if(num2 == 0){
-	//printf("erro Divi por 0\n");
-	//}else{
+	if(num1 == 0){
+	printf("erro Divi por 0\n");
+        }else{
 	resultado= num1 / num2;
-	//printf("o resultado é:%2.f", resultado);
-	//}	
+	printf("o resultado é:%.2f", resultado);
+	}	
 	}
 float valor (){
 
 	float numero; 
 		printf("Digite um numero");
 		scanf("%f",&numero);
-		return numero; 		
+		return numero;
+
 
 
 }
@@ -72,7 +73,11 @@ while(cont){
             //printf("Resultado: %.2f\n", resultado);
             break;
         case '/':
-	    div(valor(),valor());
+	    float num1;
+	    float num2;
+	    num1 = valor();
+	    num2 = valor();
+	    div (num1,num2);
             /*if (num2 != 0) {
                 resultado = num1 / num2;
                 printf("Resultado: %.2f\n", resultado);
